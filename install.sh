ed7042220b1e122a3d99e12fb769b646a83feb1b
Mark Sost <sostmark@gmail.com>
2017-05-05 10:16:29 -0400

    Revert && removal

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-curl -Lo docker-redis https://raw.githubusercontent.com/marksost/docker-redis/master/docker-redis
-chmod +x docker-redis
+curl -Lo docker-redis https://raw.githubusercontent.com/marksost/docker-redis/master/docker-redis && chmod +x docker-redis
